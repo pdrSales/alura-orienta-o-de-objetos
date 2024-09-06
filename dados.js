e5e663acaf6eb87ef9e8efe2f7f374b4745c5044
Pedro Sales <102633711+yyyyReverse@users.noreply.github.com>
2024-09-06 20:30:07 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,21 @@
+let dados = [
+    {
+        titulo : "Ronaldo Nazário (Fenômeno)",
+        descricao : "A passagem de Ronaldo Nazário pelo Corinthians foi uma das mais marcantes desua carreira. Chegando em 2009 com o peso das lesões e da idade, o Fenômeno rapidamente conquistou a Fiel Torcida com garra e experiência. Sua chegada revitalizou o time, que conquistou o Campeonato Paulista e a Copa do Brasil em 2009. Apesar de ter encerrado a carreira em 2011, Ronaldo deixou um legado inesquecível no Corinthians,sendo considerado um dos maiores ídolos da história do clube. Sua passagem pelo Timão foi marcadaporgols importantes, títulos e uma identificação única com a torcida.",
+        link : "https://pt.wikipedia.org/wiki/Ronaldo_Naz%C3%A1rio",
+        tags: "2009 copa paulista r9 fenomeno"
+    },
+    {
+        titulo : "Marcelinho Carioca",
+        descricao : "Marcelinho Carioca é considerado um dos maiores ídolos da história do Corinthians. Conhecido por sua habilidade com a bola e por seus gols de falta, ele foi um dos principais jogadores do time na década de 1990. Marcelinho conquistou diversos títulos com a camisa do Corinthians, como o Campeonato Paulista e a Copa do Brasil.",
+        link : "https://pt.wikipedia.org/wiki/Marcelinho_Carioca",
+        tags: "falta idolo 90 carioca"
+    },
+    {
+        titulo : "Sócrates Brasileiro de Souza (Doutor Sócrates)",
+        descricao : "Sócrates foi um dos maiores ídolos da história do Corinthians e um dos principais expoentes do futebol arte brasileiro. Conhecido por sua inteligência em campo e por sua liderança, ele liderou o Corinthians na conquista do Campeonato Brasileiro de 1982 e 1983.",
+        link : "https://pt.wikipedia.org/wiki/S%C3%B3crates_(futebolista)",
+        tags: "doutor capitao brasileiro magrao democracia"
+    }
+    
+];
